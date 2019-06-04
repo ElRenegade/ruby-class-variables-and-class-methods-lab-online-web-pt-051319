73691a001943e9863ea4927a550f652fb012555b
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-04 00:24:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,8 +20,7 @@ class Song
   end
 
   def self.genre_count
-    total_genres={}
-    @@genres.inject(total_genres){|k, v| v[k]+=1; v}
+    @@genres.inject(total_genres.new(0)){|k, v| v[k]+=1; v}
   end
 
   def self.artist_count
